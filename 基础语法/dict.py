e352b22e1b8938d15c9f29442af2b178e81f8e4f
Wp666 <18717792412@163.com>
2020-04-10 17:23:28 +0800

    基础回顾

--- 基础语法/dict.py
+++ 基础语法/dict.py
@@ -1,6 +1,11 @@
-#python中的dict类型(用{}括起来) 是键值对集合 相当于java中的map
+# python中的dict类型(用{}括起来) 是键值对集合 相当于java中的map
 score = {"wp":100,"wlj":90,"lhy":50}
 print(score["wp"])
 
 score["undefined"] = 10
 print(score["undefined"])
+
+if __name__ == '__main__':
+    print(score.keys())
+    print(score.values())
+    print(score.items())
--- 基础语法/list.py
+++ 基础语法/list.py
@@ -3,6 +3,17 @@ classmates = ["wp","wlj"]
 print(classmates.count("wp"))
 
 
-#python中的tuple集合类型(用()括起来)，定义后元素不可变，只能查询
+# python中的tuple集合类型(用()括起来)，定义后元素不可变，只能查询
 classmates = ("wp","wlj")
 
+if __name__ == '__main__':
+    hobbies = ["1","2","3","4"]
+    hobbies.reverse()
+    print(hobbies)
+    hobbies.sort(reverse=True)
+    print(hobbies)
+    print(hobbies.count(2))
+    # print(hobbies.index("5"))
+
+    yz = (2,)
+    print(yz)
